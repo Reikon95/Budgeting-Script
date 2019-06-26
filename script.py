849b82db70a01b7ab11c266c44061b574f773bff
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-26 08:45:19 +0100

    Update script.py

--- script.py
+++ script.py
@@ -4,3 +4,12 @@ def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
     return True
   else: 
     return False
+
+def how_much(budget, food_bill, electricity_bill, internet_bill, rent):
+  total_exp = (food_bill + electricity_bill + internet_bill + rent)
+  if (budget < total_exp):
+    return 'You are ' + str((total_exp - budget)) + ' over budget!'
+  elif (budget > total_exp:
+     return 'Nice job, you are ' + str((budget - total_exp)) + ' under budget!'
+  else:
+        return 'You are exactly at your budget - be careful!'
